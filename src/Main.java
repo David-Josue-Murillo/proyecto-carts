9a7300ee419f4b8bbed9bcf7750be599febb4190
David Murillo <david.josue.murillo23@gmail.com>
2025-11-28 13:36:12 -0500

    Refactorizar: agregar la inicialización de MainWindow en el método main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import gui.MainWindow;
+
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Proyecto-carts");
+        new MainWindow().setVisible(true);
     }
 }
